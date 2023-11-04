be25c340af398195aa8b58e13f24443b136686d3
pralay giri <pralaygiri297@gmail.com>
2023-11-04 09:54:36 +0530

    updated some feature

--- back-end/controlers/exitGroup.js
+++
@@ -1,5 +0,0 @@
-const exitGroup = (req, res) => {
-    res.status(404).send("not avalable");
-};
-
-module.exports = exitGroup;
--- back-end/routers/route.js
+++ back-end/routers/route.js
@@ -1,3 +1,4 @@
+const router = express.Router();
 const express = require("express");
 const auth = require("../meddleware/auth");
 const fileupload = require("express-fileupload");
@@ -10,29 +11,37 @@ const searchControler = require("../controlers/searchControler");
 const findUserByNumber = require("../controlers/findUserByNumber");
 const getGroupControler = require("../controlers/getGroupControler");
 const getusercredential = require("../controlers/getusercredential");
-const createGroupControler = require("../controlers/createGroupControler");
-const getallcontactControler = require("../controlers/getallcontactControler");
-const changeAboutControler = require("../controlers/changeAboutControler");
 const changeNameControler = require("../controlers/changeNameControler");
+const createGroupControler = require("../controlers/createGroupControler");
+const changeAboutControler = require("../controlers/changeAboutControler");
 const changeProfileControler = require("../controlers/changeProfileControler");
-const router = express.Router();
+const getallcontactControler = require("../controlers/getallcontactControler");
 
 router.use(fileupload());
 
+// get request
 router.get("/createuser/login", loginControler);
-router.post("/createuser/signin", signinControler);
-router.post("/contact/addcontact", auth, addContact);
 router.get("/contact/searchuser", auth, searchControler);
 router.get("/contact/getGroups", auth, getGroupControler);
 router.get("/getusercredential", auth, getusercredential);
 router.get("/getallcontacts", auth, getallcontactControler);
-router.post("/contact/createGroup", auth, createGroupControler);
-router.get("/contact/findUserByNumber", auth, findUserByNumber);
 router.get("/contact/findCommonGroup", auth, findCommonGroup);
+router.get("/contact/findUserByNumber", auth, findUserByNumber);
+
+// post request
+router.post("/createuser/signin", signinControler);
+router.post("/contact/addcontact", auth, addContact);
+router.post("/contact/createGroup", auth, createGroupControler);
+
+//delete request
 router.delete("/contact/deleteChat", auth, deleteChat);
-router.put("/contact/update/about", auth, changeAboutControler);
+
+//put request
 router.put("/contact/update/name", auth, changeNameControler);
+router.put("/contact/update/about", auth, changeAboutControler);
 router.put("/contact/update/profile", auth, changeProfileControler);
+
+//other request
 router.all("*", (req, res) => res.status(404).send("not found"));
 
 module.exports = router;
